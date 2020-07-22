a172a6387c72bee727545c7626e2946f221073ba
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-07-22 15:07:46 +0200

    After changes in style.css
    removed some unused code from style.css

--- docs/ToDo.md
+++ docs/ToDo.md
@@ -1,5 +1,6 @@
 # important things todo
 - create some effects
 - highlight text in the website with another color
+- fix media query (higher px)
 
 # done
--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const icon = document.getElementById('iconToToggle');
 
 navToggle.addEventListener('click', () => {
+    document.getElementById('iconToToggle').classList.toggle('fa-times-circle');
     document.body.classList.toggle('nav-open');
-
 });
 
 navLinks.forEach(link => {
