feeab7368a734a88ca9ef46037559a2763eb209a
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-08-17 13:19:57 +0200

    Implement back to top button
    Adds back to top feature with
    a rocket that launched up to the space.

--- docs/ToDo.md
+++ docs/ToDo.md
@@ -5,6 +5,8 @@
 # notes 
 
 # to implement
+- remove cursor rocket and use the default cursor
+- back to top in footer
 - favicon in SERP page
 - make the website discoverable only with "ivan pesenti" seach words
 - make the site the first result in SERP
--- js/index.js
+++ js/index.js
@@ -1,3 +1,40 @@
+jQuery(window).scroll(function(){
+    if(jQuery(window).scrollTop()<50){
+        jQuery('#rocketmeluncur').slideUp(500);
+    }else{
+        jQuery('#rocketmeluncur').slideDown(500);
+    }
+
+var ftrocketmeluncur = jQuery("#ft")[0] ? jQuery("#ft")[0] : jQuery(document.body)[0];
+var scrolltoprocketmeluncur = $('#rocketmeluncur')[0];
+var viewPortHeightrocketmeluncur = parseInt(document.documentElement.clientHeight);
+var scrollHeightrocketmeluncur = parseInt(document.body.getBoundingClientRect().top);
+var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
+var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
+if (basewrocketmeluncur < 1000) {
+var leftrocketmeluncur = parseInt(fetchOffset(ftrocketmeluncur)['left']);
+leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
+scrolltoprocketmeluncur.style.left = ( basewrocketmeluncur + leftrocketmeluncur ) + 'px';
+} else {
+scrolltoprocketmeluncur.style.left = 'auto';
+scrolltoprocketmeluncur.style.right = '1.75em';
+}
+})
+
+jQuery('#rocketmeluncur').click(function(){
+    jQuery("html, body").animate({ scrollTop: '0px',display:'none'},{
+            duration: 600,  
+            easing: 'linear'
+        });
+    
+    var self = this;
+    this.className += ' '+"launchrocket";
+    setTimeout(function(){
+      self.className = 'showrocket';
+    },800)
+});
+
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 // const icon = document.getElementById('iconToToggle'); //TODO if not useful remove
