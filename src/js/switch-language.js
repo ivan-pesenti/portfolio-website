00492e0d4b280796c730b81612457fb5a9425861
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-12-31 14:22:56 +0100

    switch languages from js file to json file it loaded by default by webpack 5

--- src/js/switch-language.js
+++ src/js/switch-language.js
@@ -1,8 +1,8 @@
-import { data } from "./language-db";
-
+// import { data } from "./language-db";
+import jsonData from "../assets/data/language-db.json";
 export class SwitchLanguage {
 
-    constructor(){
+    constructor() {
         // item to change based on the language
         this.homeNavbar = document.querySelector('.nav__list').getElementsByTagName('li')[0].getElementsByTagName('a')[0];
         this.aboutNavbar = document.querySelector('.nav__list').getElementsByTagName('li')[1].getElementsByTagName('a')[0];
@@ -23,10 +23,10 @@ export class SwitchLanguage {
         this.bodyServicesLast = document.querySelector('.services').getElementsByTagName('p')[2];
         this.titleSkills = document.querySelector('.section__title--skills');
         this.subtitleSkills = document.querySelector('.section__subtitle--skills');
-        
+
         // effect to open/close language switch section
         this.alienToggle = document.querySelector('.alien-toggle');
-        this.alienLinks = document.querySelectorAll('.alien__link');      
+        this.alienLinks = document.querySelectorAll('.alien__link');
     }
 
     setupNavbar() {
@@ -34,7 +34,7 @@ export class SwitchLanguage {
         this.alienToggle.addEventListener('click', () => {
             document.getElementById('alienToToggle').classList.toggle('fa-times-circle');
             // hide burger menu when switch language menu is opened and viceversa
-            var navToggle = document.querySelector(".nav-toggle");    
+            var navToggle = document.querySelector(".nav-toggle");
             document.body.classList.toggle('alien-open');
             if (document.body.className.indexOf("alien-open") > -1) {
                 navToggle.style['z-index'] = 0;
@@ -45,38 +45,47 @@ export class SwitchLanguage {
     }
 
     addLinksOnButton() {
-         // loop on link in order to add/remove class active from buttons    
-         this.alienLinks.forEach(al => {
-            al.addEventListener('click', () => {        
-        
+        // loop on link in order to add/remove class active from buttons    
+        this.alienLinks.forEach(al => {
+            al.addEventListener('click', () => {
+
                 // get selected language        
                 const languageSelected = al.getAttribute('language');
-        
+
+                // get text from the selected language
+                const languageData = jsonData.filter(obj => {
+                    var result;
+                    if (obj.language === languageSelected) {
+                        result = obj.data
+                    }
+                    return result;
+                })[0].data;
+
                 // set innerHtml of the element
-                this.homeNavbar.innerHTML = data[languageSelected].homeNavbar;
-                this.aboutNavbar.innerHTML = data[languageSelected].aboutNavbar;
-                this.servicesNavbar.innerHTML = data[languageSelected].servicesNavbar;
-                this.skillsNavbar.innerHTML = data[languageSelected].skillsNavbar;
-                this.titleIntro.innerHTML = data[languageSelected].titleIntro;
-                this.titleAbout.innerHTML = data[languageSelected].titleAbout;
-                this.subtitleAbout.innerHTML = data[languageSelected].subtitleAbout;
-                this.bodyAboutFirst.innerHTML = data[languageSelected].bodyAboutFirst;
-                this.bodyAboutSecond.innerHTML = data[languageSelected].bodyAboutSecond;
-                this.bodyAboutFinal.innerHTML = data[languageSelected].bodyAboutLast;
-                this.titleServices.innerHTML = data[languageSelected].titleServices;
-                this.subtitleServicesFirst.innerHTML = data[languageSelected].subtitleServicesFirst;
-                this.bodyServicesFirst.innerHTML = data[languageSelected].bodyServicesFirst;
-                this.subtitleServicesSecond.innerHTML = data[languageSelected].subtitleServicesSecond;
-                this.bodyServicesSecond.innerHTML = data[languageSelected].bodyServicesSecond;
-                this.subtitleServicesLast.innerHTML = data[languageSelected].subtitleServicesLast;
-                this.bodyServicesLast.innerHTML = data[languageSelected].bodyServicesLast;
-                this.titleSkills.innerHTML = data[languageSelected].titleSkills;
-                this.subtitleSkills.innerHTML = data[languageSelected].subtitleSkills;
-        
+                this.homeNavbar.innerHTML = languageData.homeNavbar;
+                this.aboutNavbar.innerHTML = languageData.aboutNavbar;
+                this.servicesNavbar.innerHTML = languageData.servicesNavbar;
+                this.skillsNavbar.innerHTML = languageData.skillsNavbar;
+                this.titleIntro.innerHTML = languageData.titleIntro;
+                this.titleAbout.innerHTML = languageData.titleAbout;
+                this.subtitleAbout.innerHTML = languageData.subtitleAbout;
+                this.bodyAboutFirst.innerHTML = languageData.bodyAboutFirst;
+                this.bodyAboutSecond.innerHTML = languageData.bodyAboutSecond;
+                this.bodyAboutFinal.innerHTML = languageData.bodyAboutLast;
+                this.titleServices.innerHTML = languageData.titleServices;
+                this.subtitleServicesFirst.innerHTML = languageData.subtitleServicesFirst;
+                this.bodyServicesFirst.innerHTML = languageData.bodyServicesFirst;
+                this.subtitleServicesSecond.innerHTML = languageData.subtitleServicesSecond;
+                this.bodyServicesSecond.innerHTML = languageData.bodyServicesSecond;
+                this.subtitleServicesLast.innerHTML = languageData.subtitleServicesLast;
+                this.bodyServicesLast.innerHTML = languageData.bodyServicesLast;
+                this.titleSkills.innerHTML = languageData.titleSkills;
+                this.subtitleSkills.innerHTML = languageData.subtitleSkills;
+
                 // remove alien-open class in order to collapse it to the left 
                 document.body.classList.remove('alien-open');
                 document.getElementById('alienToToggle').classList.toggle('fa-times-circle');
-        
+
                 // unhide burger menu when the switch language menu is closed by selecting a language
                 var navToggle = document.querySelector(".nav-toggle");
                 navToggle.style['z-index'] = 1000;
