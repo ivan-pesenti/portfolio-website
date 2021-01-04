79de5bf2b9f0da20130d75a36475ef9312f4065c
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-01-04 10:10:52 +0100

    set up properly the source map to use inline-source-map only in development env

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const common = require("./webpack.common");
-const {merge} = require("webpack-merge");
+const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
     mode: "development",
@@ -8,6 +8,6 @@ module.exports = merge(common, {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         publicPath: "/"
-    }, 
-    devtool: 'source-map'
+    },
+    devtool: 'inline-source-map'
 });
